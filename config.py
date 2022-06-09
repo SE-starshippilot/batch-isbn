e780d63ec2822a216d8315b16ed6dc21f883e055
Tianhao <30402585+SE-starshippilot@users.noreply.github.com>
2022-06-09 16:25:57 +0800

    updated config file

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-EDITION_ATTRIBUTES = ["publishers", "publish_date", "physical_format", "isbn_13"],
-BOOK_ATTRIBUTES =["title", "author_name", "edition_key"],
-SHEETINDEX = 0,
-LOWBOUND = 0.3,
-HIGHBOUND = 0.8,
-BOOK_QUERY_URL = "https://openlibrary.org/search.json?title=",
-EDITION_QUERY_URL = "https://openlibrary.org/books/",
-MAXIMUM_TRIALS = 3,
-YEAR_PATTERN = "[0-9]{4}",
+EDITION_ATTRIBUTES = ["publishers", "publish_date", "physical_format", "isbn_13"]
+BOOK_ATTRIBUTES =["title", "author_name", "edition_key"]
+SHEETINDEX = 0
+LOWBOUND = 0.3
+HIGHBOUND = 0.8
+BOOK_QUERY_URL = "https://openlibrary.org/search.json?title="
+EDITION_QUERY_URL = "https://openlibrary.org/books/"
+MAXIMUM_TRIALS = 3
+YEAR_PATTERN = "[0-9]{4}"
 PHYSICAL_FORMAT_MAP={
     "paperback": 1,
     "mass market paperback": 0.8,
