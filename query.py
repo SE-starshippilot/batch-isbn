287ab51af318404be1dd9395302f2fcc142ce604
Tianhao <30402585+SE-starshippilot@users.noreply.github.com>
2022-06-09 10:47:41 +0800

    factorized getBookPage and getEditionPage into accessPage

---
+++ query.py
@@ -0,0 +1,57 @@
+import json
+import requests
+import numpy as np
+from bs4 import BeautifulSoup
+
+conf = None
+
+def formatTitle(title):
+    f_title = title
+    return f_title
+
+def accessPage(baseURL:str, id:str, postfix:str='')->json:
+    """
+    access a page from a root node specified by id and postfix
+    returns a json object
+    """
+    pageURL = baseURL + id + postfix
+    page = requests.get(pageURL)
+    assert page.ok
+    return json.loads(page.text)
+
+def getBook(bookTitle):
+    """
+    returns the most similar book found on Open Library as a json object
+    """
+    # url = conf['BASE_URL'] + f_title
+    page = requests.get('https://openlibrary.org/search.json?title=The+Elements+of+Styles&author=William')
+    return json.loads(page.text)['docs'][0]
+
+def getEdition(editionID):
+    """
+    return information about a specific edition
+    """
+    url = conf['EDITION_QUERY_URL'] + editionID + '.json?'
+    page = requests
+    return
+
+def debug():
+    global conf
+    # docs = getBook("The+Elements+of+Styles")
+    with open('config.json', 'r') as jf:
+        conf = json.load(jf)
+    # There could be more than one matches, and we pick the most similar one as the result
+    try:
+        bookInfo = accessPage(conf['BOOK_QUERY_URL'], "The+Elements+of+Styles")['docs'][0]
+        with open('sampleBookInfo.json', 'w') as f:
+            json.dump(bookInfo, f, indent=2)
+        firstEdition = bookInfo['edition_key'][1]
+        editionInfo = accessPage(conf['EDITION_QUERY_URL'], 'OL31438449M', postfix='.json?')
+        with open('sampleEditionInfo.json', 'w') as f:
+            json.dump(editionInfo, f, indent=2)
+    except AssertionError:
+        print('Ouch')
+
+
+if __name__ == '__main__':
+    debug()
