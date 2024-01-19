05a67433773aca1cf1d2305fae20ec72ad24731a
Tianhao <120090472@link.cuhk.edu.cn>
2024-01-19 19:21:59 +0800

    changed destructor and shortened the waiting time for speed

--- page_scrapper.py
+++ page_scrapper.py
@@ -8,7 +8,7 @@ class PageScrapper:
     """
     A class that scrapes a webpage and returns a BeautifulSoup object.
     """
-    def __init__(self, min_sleep_interval:float=2.0, max_sleep_interval:float=8.0, trial_times:int=3)->None:
+    def __init__(self, min_sleep_interval:float=1.0, max_sleep_interval:float=4.0, trial_times:int=3)->None:
         self.__trial_times = trial_times
         self.__sleep_interval = lambda : time.sleep(random.random()*(max_sleep_interval - min_sleep_interval) + min_sleep_interval)
         self.__driver = webdriver.Edge()
@@ -18,7 +18,6 @@ class PageScrapper:
         for _ in range(self.__trial_times):
             self.__sleep_interval()
             self.__driver.get(url) 
-            self.__sleep_interval()
             self.__driver.execute_script(f"window.scrollTo(0, {random.randrange(0, 1000, 1)});")
             state = self.__driver.execute_script("return document.readyState")
             if not(state == "complete"): continue
@@ -30,9 +29,12 @@ class PageScrapper:
         return self
     
     def __exit__(self, exc_type, exc_value, traceback):
-        self.__driver.quit()
+        self.quit()
         if exc_type is not None:
             print(exc_type, exc_value, traceback)
             return False
         return True
+    
+    def quit(self):
+       self.__driver.quit() 
 
